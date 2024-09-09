478bb45e58d29323cef61c32b629eeedd811c8cb
sathishkumar <sathis.m76@gmail.com>
2024-09-09 19:00:43 +0530

    resume updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { MdFileDownload } from "react-icons/md";
 import code from "../assets/images/coding.png";
 // import pic from "../assets/images/sathish.png"
 import { Element } from "react-scroll";
+import "animate.css/animate.compat.css";
+import ScrollAnimation from "react-animate-on-scroll";
 // import wave from "../assets/images/wave.svg"
 
 export function Home() {
@@ -14,18 +16,17 @@ export function Home() {
       <div className="personal-info-container">
         <div className="personal-info">
           <div>
-            <div className="personal-name">Hey! </div>
+            <div className="personal-name animate__animated animate__fadeInLeft">
+              Hey!{" "}
+            </div>
             <div className="personal-name">I'm Sathishkumar M</div>
 
             <div className="personal-role">
               <h4>Front-end Developer</h4>
             </div>
             <a
-              href={require("../pdf/Sathishkumar_resume.pdf")}
-             
-             
+              href={require("../pdf/sathishkumar_fullstack_resume.pdf")}
               download="sathishkumar-resume.pdf"
-              
             >
               <div className="resume-btn">
                 <span className="resume-txt"> Resume</span>
